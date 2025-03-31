c749ae38c1b78415b1c12669a44f96dadb2b8354
tohruss <taraskind6@gmail.com>
2025-03-31 14:32:01 +0700

    NAME_PATTERN(update)

--- src/Validation.php
+++ src/Validation.php
@@ -5,6 +5,6 @@ namespace Collect;
 class Validation
 {
     public const PASSWORD = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/';
-    public const NAME_PATTERN = '/^[\p{L} \-\'’]+$/u';
+    public const NAME_PATTERN = '/^[А-Яа-яЁё\s\-\'’]+$/u';
     public const HOURS_PATTERN = '/^([01]\d|2[0-3]):([0-5]\d)$/';
 }
