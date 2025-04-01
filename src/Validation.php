ec4c6edc5308d28ef1d007e897b4bc0658420755
tohruss <taraskind6@gmail.com>
2025-04-01 10:22:58 +0700

    ADDRESS_PATTERN(update)

--- src/Validation.php
+++ src/Validation.php
@@ -7,4 +7,5 @@ class Validation
     public const PASSWORD = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/';
     public const NAME_PATTERN = '/^[А-Яа-яЁё\s\-\'’]+$/u';
     public const HOURS_PATTERN = '/^([01]\d|2[0-3]):([0-5]\d)$/';
+    public const ADDRESS_PATTERN = '/^[А-Яа-яЁё0-9\s\-,.\/]+$/u';
 }
